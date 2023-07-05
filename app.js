944e97e648de59cd212b43b01656a4a86985784b
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-07-05 13:40:44 -0500

    updating files
    Updating files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// DOM elements
+
 const booksLink = document.getElementById('books-link');
 const addBookLink = document.getElementById('add-book-link');
 const contactLink = document.getElementById('contact-link');
@@ -8,13 +8,13 @@ const contactSection = document.getElementById('contact-section');
 const addBookForm = document.getElementById('add-book-form');
 const booksList = document.getElementById('books-list');
 
-// Event listeners
+
 booksLink.addEventListener('click', showBooksSection);
 addBookLink.addEventListener('click', showAddBookSection);
 contactLink.addEventListener('click', showContactSection);
 addBookForm.addEventListener('submit', handleFormSubmit);
 
-// Functions to show or hide sections
+
 function showBooksSection() {
   booksSection.style.display = 'block';
   addBookSection.style.display = 'none';
@@ -33,17 +33,17 @@ function showContactSection() {
   contactSection.style.display = 'block';
 }
 
-// Function to handle form submission
-function handleFormSubmit(event) {
-  event.preventDefault(); // Prevent the default form submission
 
-  // Access form input values
+function handleFormSubmit(event) {
+  event.preventDefault(); 
+
+ 
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const genre = document.getElementById('genre').value;
   const year = document.getElementById('year').value;
 
-  // Create a new book element
+  
   const bookElement = document.createElement('div');
   bookElement.classList.add('book');
   bookElement.innerHTML = `
@@ -54,18 +54,18 @@ function handleFormSubmit(event) {
     <button class="remove-button">Remove</button>
   `;
 
-  // Append the new book element to the books list
+  
   booksList.appendChild(bookElement);
 
-  // Clear form inputs after submission
+  
   addBookForm.reset();
 
-  // Add event listener to the remove button
+  
   const removeButton = bookElement.querySelector('.remove-button');
   removeButton.addEventListener('click', () => {
     bookElement.remove();
   });
 }
 
-// Initial section to display
+
 showBooksSection();
